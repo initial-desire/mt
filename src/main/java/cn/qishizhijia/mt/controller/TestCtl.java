ad70fbae7dd0568bcfe1b7a2e94ccdad940fe89b
alex <772903227@qq.com>
2016-02-25 15:48:41 +0800

    5

--- src/main/java/cn/qishizhijia/mt/controller/TestCtl.java
+++ src/main/java/cn/qishizhijia/mt/controller/TestCtl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.qishizhijia.mt.pojo.MtStaff;
@@ -16,8 +17,9 @@ public class TestCtl {
 	@Autowired
 	private TestService testService;
 	
-	@RequestMapping(value="mytest/{ttt}")
+	@RequestMapping(value="mytest/getTest.do", method=RequestMethod.GET)
 	public @ResponseBody List<MtStaff> test(){
+		System.out.println("...");
 		return testService.test();
 	}
 	
